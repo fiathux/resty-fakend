d8be11f20f3644a5ed87c6f389b8a245c1c06881
fiathux Su <fxcat@FxcatPrelude.local>
2020-02-13 18:26:27 +0800

    change depend packags to local distance

--- package_template/base/install.sh
+++ package_template/base/install.sh
@@ -6,8 +6,8 @@ echo "Start install"
 # install openresty
 echo "============= Install OpenResty ============="
 cd /home/depsource
-curl https://openresty.org/download/openresty-1.15.8.2.tar.gz>openresty-1.15.8.2.tar.gz
-curl http://www.zlib.net/zlib-1.2.11.tar.gz>zlib-1.2.11.tar.gz
+curl -L http://gitlab.droi.cn/frame/public-images/raw/master/resty_youliao/openresty-1.15.8.2.tar.gz>openresty-1.15.8.2.tar.gz
+curl -L http://gitlab.droi.cn/frame/public-images/raw/master/resty_youliao/zlib-1.2.11.tar.gz>zlib-1.2.11.tar.gz
 tar xvzf zlib-1.2.11.tar.gz
 tar xvzf openresty-1.15.8.2.tar.gz
 cd openresty-1.15.8.2
@@ -15,14 +15,14 @@ cd openresty-1.15.8.2
 make install
 echo "============= Install Lua ============="
 cd /home/depsource
-curl https://www.lua.org/ftp/lua-5.1.5.tar.gz>lua-5.1.5.tar.gz
+curl -L http://gitlab.droi.cn/frame/public-images/raw/master/resty_youliao/lua-5.1.5.tar.gz>lua-5.1.5.tar.gz
 tar xvzf lua-5.1.5.tar.gz
 cd lua-5.1.5
 make linux
 make install
 echo "============= Install Luarocks ============="
 cd /home/depsource
-curl -L http://luarocks.github.io/luarocks/releases/luarocks-3.0.3.tar.gz>luarocks-3.0.3.tar.gz
+curl -L http://gitlab.droi.cn/frame/public-images/raw/master/resty_youliao/luarocks-3.0.3.tar.gz>luarocks-3.0.3.tar.gz
 tar xvzf luarocks-3.0.3.tar.gz
 cd luarocks-3.0.3
 ./configure --with-lua=/usr/local/
@@ -30,7 +30,7 @@ make install
 luarocks install lua-resty-http
 echo "============= Install Mongo-C-Driver ============="
 cd /home/depsource
-curl -L https://github.com/mongodb/mongo-c-driver/releases/download/1.13.0/mongo-c-driver-1.13.0.tar.gz>mongo-c-driver-1.13.0.tar.gz
+curl -L http://gitlab.droi.cn/frame/public-images/raw/master/resty_youliao/mongo-c-driver-1.13.0.tar.gz>mongo-c-driver-1.13.0.tar.gz
 tar xvzf mongo-c-driver-1.13.0.tar.gz
 cd mongo-c-driver-1.13.0
 cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF
@@ -47,3 +47,4 @@ mkdir /home/backup
 mv /home/install.sh /home/backup
 echo "=========================================="
 echo "Complete!"
+
